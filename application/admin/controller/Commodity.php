0b596bee9827fb1d13651849723a24100a0418cf
Hancook <827111587@qq.com>
2019-07-05 11:26:30 +0800

    '111'

--- application/admin/controller/Commodity.php
+++ application/admin/controller/Commodity.php
@@ -219,57 +219,6 @@ class Commodity extends Base
 
 
 
-    public function save_attrs()
-    {
-        if (request()->isPost()) {
-            $data = request()->post();
-            $key = json_decode($data['key'], true);
-            $value = json_decode($data['value'], true);
-            $item_id = 1;
-            $key_id = [];
-            ItemAttrKey::where(['item_id' => $item_id])->delete();
-            foreach ($key as $k) {
-                $attr_key = ItemAttrKey::where(['attr_name' => $k, 'item_id' => $item_id])->find();
-                if (!$attr_key) {
-                    $attr_key = new ItemAttrKey();
-                    $attr_key->attr_name = $k;
-                    $attr_key->item_id = $item_id;
-                    $attr_key->save();
-                }
-                $key_id[] = $attr_key->attr_key_id;
-            }
-
-            $tm_v_in = [];
-            $tm_v = [];
-            ItemAttrVal::where(['item_id' => $item_id])->delete();
-            foreach ($value as $key => $v) {
-                $attr_key_id = $key_id[$key];
-                foreach ($v as $v1) {
-                    $attr_value = ItemAttrVal::where(['attr_value' => $v1, 'attr_key_id' => $attr_key_id])->find();
-                    if (!$attr_value) {
-                        $attr_value = new ItemAttrVal();
-                        $attr_value->attr_key_id = $attr_key_id;
-                        $attr_value->attr_value = $v1;
-                        $attr_value->item_id = $item_id;
-                        $attr_value->save();
-                    }
-                    $tm_v[] = $attr_value->symbol;
-
-
-                }
-//                $tm_v[]=$tm_v_in;
-            }
-
-            $this->success('请求成功', ['key' => $key_id, 'value' => $tm_v]);
-
-        }
-
-        $this->success('请求成功');
-
-    }
-
-
-
 
 
 
@@ -292,8 +241,8 @@ class Commodity extends Base
         return $this->fetch();
     }
 
-    //添加商品属性
 
+    //添加商品属性
     public function category_add(){
         if(request()->isAjax()){
             $param=$this->request->param();
