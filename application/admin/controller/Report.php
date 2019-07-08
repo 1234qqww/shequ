6cd95bc88f110d5cacc24aac54172caa547330f2
1234qqww <1779638566@qq.com>
2019-07-08 11:30:57 +0800

    'a'

--- application/admin/controller/Report.php
+++ application/admin/controller/Report.php
@@ -2,14 +2,22 @@
 namespace app\admin\controller;
 use think\Controller;
 use think\Request;
-class Admin extends Base
+include __DIR__ . '../../../vendor/workerman/Autoloader.php';
+
+// 创建一个Worker监听2345端口，使用http协议通讯
+$http_worker = new Worker("http://0.0.0.0:2345");
+
+// 启动4个进程对外提供服务
+$http_worker->count = 4;
+class Report extends Base
 {
+
     public function _initialize(){
         parent::_initialize();
     }
-    public function index()
+    public function report()
     {
-        return $this->fetch();
+        return $this->fetch('index');
     }
 
 
